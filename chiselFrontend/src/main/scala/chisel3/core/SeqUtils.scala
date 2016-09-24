a3fff651c8eed7b9c34901256a3664941533349a
ducky <richard.lin@berkeley.edu>
2016-09-23 17:26:36 -0700

    Test dummy transform

--- chiselFrontend/src/main/scala/chisel3/core/SeqUtils.scala
+++ chiselFrontend/src/main/scala/chisel3/core/SeqUtils.scala
@@ -10,7 +10,6 @@ private[chisel3] object SeqUtils {
   /** Equivalent to Cat(r(n-1), ..., r(0)) */
   def asUInt[T <: Bits](in: Seq[T]): UInt = macro SourceInfoTransform.inArg
 
-  @chisel3.internal.naming.dump
   def do_asUInt[T <: Bits](in: Seq[T])(implicit sourceInfo: SourceInfo): UInt = {
     if (in.tail.isEmpty) {
       in.head.asUInt
